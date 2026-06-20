9fb543efd68a0bd8c7798d1834b2109663c51119
Arthur Malheiros <arthurbox360@gmail.com>
2026-06-20 14:59:30 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'avaliacaoFirebase',
+  webDir: 'www'
+};
+
+export default config;
